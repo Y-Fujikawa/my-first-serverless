fad46023fd2d9407645a8b3b5cb31b6e481b4b5f
Yasuyuki Fujikawa <fujikawa0@gmail.com>
2016-08-28 15:49:25 +0900

    Add Access DynamoDB to function hender.hello
    TODO
    Access DynamoDB do set IAM role automatic.

--- handler.js
+++ handler.js
@@ -1,11 +1,21 @@
 'use strict';
 
+var AWS = require("aws-sdk");
+var dynamo = new AWS.DynamoDB.DocumentClient();
+
 // Your first function handler
 module.exports.hello = (event, context, cb) => {
-  console.log("☆☆");
-  console.log(event);
-  console.log("☆☆");
-  cb(null, { message: 'Go Serverless v1.0! Your function executed successfully!', hoge1: event.key1 });
+  dynamo.scan({TableName : "my-first-serverless"}, function(err, data) {
+    if (err) {
+      console.log(err); // エラー時
+    } else {
+      console.log("☆☆");
+      console.log(event);
+      console.log(data);
+      console.log("☆☆");
+      cb(null, { message: 'Go Serverless v1.0! Your function executed successfully!', hoge1: event.key1, dynamo: data });
+    }
+  });
 };
 
 // You can add more handlers here, and reference them in serverless.yml
